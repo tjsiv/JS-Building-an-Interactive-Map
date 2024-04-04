999db7d33d3c35df27e521b7ee7e31daf89cf0a2
tjsiv <tjsivelsjr@gmail.com>
2024-04-03 19:57:56 -0700

    added map first marker and 1st polygon

--- public/app.js
+++ public/app.js
@@ -3,11 +3,20 @@
 
 
 // Create map
+// Create map:                                                       
+const myMap = L.map('map', {
+    center: [48.868672, 2.342130],
+    zoom: 12,
+});
 
 
 
 
 // Add openstreetmap tiles
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    minZoom: '15',
+}).addTo(myMap)
 
 
 
@@ -16,11 +25,20 @@
 
 // Create and add a geolocation marker
 
+const marker = L.marker([48.87007, 2.346453])
+marker.addTo(myMap).bindPopup('<p1><b>The Hoxton, Paris</b></p1>').openPopup()
 
 
 
 // Draw the 2nd arrondissement
 
+var polygon = L.polygon([
+    [48.863368120198004, 2.3509079846928516],
+    [48.86933262048345, 2.3542531602919805],
+    [48.87199261164275, 2.3400569901592183],
+    [48.86993336274516, 2.3280142476578813],
+    [48.86834104280146, 2.330308418109664]
+]).addTo(myMap);
 
 
 
